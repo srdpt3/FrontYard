8100bdc97c1049c524e85bc5e90a73caa60aba4d
srdpt3 <srdpt3@gmail.com>
2015-08-10 21:06:06 -0400

    Initial Commit

---
+++ Swapit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swapit
+//
+//  Created by Dustin Yang on 8/10/15.
+//  Copyright (c) 2015 Dustin Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
