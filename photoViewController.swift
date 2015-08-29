d27367ac562e5731601ae13d70a12e568bbafb40
srdpt3 <srdpt3@gmail.com>
2015-08-29 17:06:36 -0400

    with new tabbar installed

---
+++ photoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  photoViewController.swift
+//  Swapit
+//
+//  Created by Dustin Yang on 8/27/15.
+//  Copyright (c) 2015 Dustin Yang. All rights reserved.
+//
+
+import UIKit
+
+class photoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
